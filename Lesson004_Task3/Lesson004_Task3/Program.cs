175550fbdc0eba02551430cee9884d10781d5c85
Олександр Дибач <alex.dybach.2014@i.ua>
2020-11-05 16:06:06 +0200

    Done lesson004 task3

---
+++ Lesson004_Task3/Lesson004_Task3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Lesson004_Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Marco\nAnswer:");
+            string answer = Console.ReadLine();
+            if (answer == "polo" || answer == "Polo" || answer == "POLO" || answer == "pOLO")
+                Console.WriteLine("Correct Answer!");
+            else if (answer == "" || answer == " ")
+                Console.WriteLine("Empty line!");
+            else
+                Console.WriteLine("Incorrect!");
+
+        }
+    }
+}
